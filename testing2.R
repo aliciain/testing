10dc2a1e5aaf6d46364e3edd623a4a2acd696962
aliciain <aliciain@proton.me>
2026-06-24 15:54:54 -0500

    Added code to find averages per category by species.

--- testing2.R
+++ testing2.R
@@ -1,1 +1,14 @@
-print("This file is on github.")
+# Import relevant data (Iris built-in data for this case.)
+iris_data <- data.frame(iris)
+
+# Use dplyr library to simplify functions
+library(dplyr)
+
+# Find unique species of Iris in the dataset
+unique_species <- unique(iris_data$Species)
+
+# Determine the average petal length for each species
+for (species in unique_species) {
+  newdf <- iris_data %>% filter(Species == species) %>% select(-Species)
+  cat(species, colMeans(newdf[sapply(newdf, is.numeric)]), " ")
+}
